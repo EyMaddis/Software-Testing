1a4d1243d937610da0960315d82fa677d45b0c4a
Mathis Neumann <github@madd.is>
2014-11-12 15:52:49 +0200

    done - Assingment 1 handed in

---
+++ HandIn-Assignment 1/Assignment 1 - part 2/src/fi/utu/library/LibraryTest.java
@@ -0,0 +1,14 @@
+package fi.utu.library;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LibraryTest {
+
+	@Test
+	public void testSortByBasePrice() {
+		fail("Not yet implemented");
+	}
+
+}
